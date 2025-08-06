7eadf5ce4404bc676a726f917c5a4e5a7802feeb
tsushima aki <aki1120tsushima@gmail.com>
2025-08-06 19:32:17 +0900

    first commit

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function add()
+    {
+        return view('add');
+    }
+
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+
+    public function edit(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+    
+    public function update(AuthorRequest $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Author::find($request->id)->update($form);
+        return redirect('/');
+    }
+
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delate', ['author' => $author]);
+    }
+
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
+
+    public function verror()
+    {
+        return view('verror');
+    }
+}
