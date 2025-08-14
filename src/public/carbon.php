4866afbd5dc6ba371de9f2e4a1ed63307443578b
tsushima aki <aki1120tsushima@gmail.com>
2025-08-14 17:31:54 +0900

    更新3

---
+++ src/public/carbon.php
@@ -0,0 +1,13 @@
+<?php
+require '../vendor/autoload.php';
+
+use Carbon\Carbon;
+
+$dt = Carbon::now();
+echo $dt->year;
+
+$dt = Carbon::now();
+echo $dt->addYear();
+
+$dt = Carbon::now();
+echo $dt->subYear();
